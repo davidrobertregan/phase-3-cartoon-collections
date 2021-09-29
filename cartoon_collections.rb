a26e5a8bb793224f8a92b68ef11fb8f041cb3f5a
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-09-29 15:42:17 -0500

    commit tree 16860d8dc222671c0b8ef067dd7bc77bdc13a002

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+
+def roll_call_dwarves array
+  array.each_with_index.map { |e, i| puts "#{i + 1}. #{e}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet array
+  array.map {|e| "#{e}!".capitalize }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls array
+  array.any? { |e| e.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese array
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find { |e| cheese_types.include? e }
 end
+
+
+array = ["Doc", "Dopey", "Bashful", "cheddar", "Grumpy"]
+
+find_the_cheese array
